f9c21661c4c97a53fba6e332086ae6bfb74f9946
Yamini Priya <yaminipriyamr@gmail.com>
2026-08-01 20:13:00 +0530

    Update engine package init

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,1 +1,4 @@
-# Engine package initialization
+"""CIFR-LitterDetect engine module."""
+from .unfreeze_schedule import get_freeze_layers, make_unfreeze_callback, apply_finetune_strategy
+
+__all__ = ["get_freeze_layers", "make_unfreeze_callback", "apply_finetune_strategy"]
